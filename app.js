ac188cad6fef26cf0011054dee1556caf7f774f3
elogeek <elodiechristin@gmail.com>
2022-12-28 11:00:33 +0100

    suite game (create food)

--- app.js
+++ app.js
@@ -25,23 +25,39 @@ let game = new Phaser.Game(config);
 let neko;
 let cursors;
 let background;
+let food;
 
 function preload() {
     this.load.image('background', "assets/img/background.jpeg")
     this.load.image('neko', 'assets/img/neko.png');
+    this.load.image('food', 'assets/img/food.png')
 }
 
 function create() {
+    //const camera = this.cameras.main;
+    //camera.setBounds(0, 0, 800, 600);
+
     neko = this.physics.add.image(100, 100, 'neko');
     neko.body.collideWorldBounds = true;
+    // Set the order in which sprites are displayed on the stage, (the cat is in front)
     neko.setDepth(10);
     cursors = this.input.keyboard.createCursorKeys();
 
     // create background of game
     background = this.add.image(0, 0, "background");
     background.setOrigin(0, 0);
+    // Resize game image size
     background.setDisplaySize(800, 600);
     background.setScrollFactor(0);
+
+    // Create food
+    // Generates random coordinates between 0 and 800 for the X axis and between 0 and 600 for the Y axis
+    const x = Phaser.Math.Between(0, 800);
+    const y = Phaser.Math.Between(0, 600);
+    // Creates a food sprite at the generated random position
+    food = this.add.image(x, y, 'food');
+    food.setDepth(5);
+
 }
 
 function update() {
@@ -58,4 +74,8 @@ function update() {
     if(cursors.left.isDown){
         neko.setVelocity(-300, 0)
     }
+    // Food appears every 5 seconds
+    food = setInterval(create, 5000);
 }
+
+
